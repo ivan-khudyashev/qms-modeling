4880adab4538f06940255592f212603ddeb45990
Khudyashev Ivan <bahek1983@gmail.com>
2018-11-19 10:40:20 +0700

    Save commit. Work and Test Random Generator
    Remark: it is necessary dynamically define parameters (step_count, step_accuracy)
    depending on CDF and it's possible absolute value.
    If its value can realize too big we must more roughly step_accuracy and more
    greater step_count
    But if its value can realize only small valus then we must define more
    accurately value step_accuracy and decrease step_count

--- lib/random_by_cdf.py
+++ lib/random_by_cdf.py
@@ -1,45 +1,6 @@
-from numpy.random import exponential as exp_dist
 from numpy.random import random as uniform_dist
-from scipy.stats import norm as norm_distribution
-import math
 
-
-def B_factory(gamma):
-    """Factory for defining distribution function B for distribute
-    serviced time intervals
-
-    Parameters
-    ----------
-    gamma : number
-        TODO: define this parameter
-
-    Returns
-    -------
-    function-closure
-        function B with defined parameter <gamma>
-    """
-    def B_func_body(y):
-        """Distribution function represented in analitical form.
-        This function define some Random value based by Distribution function
-        Parameters
-        ----------
-        y : number
-            TODO: define this parameter
-        Returns
-        number
-            Time-interval necessary for service request
-        """
-        return y**gamma/(1.0 + y**gamma)
-    return B_func_body
-
-def Exp_factory(lambda_p):
-    def Exp_func_body(y):
-        if y < 0.0:
-            return 0.0
-        return 1 - math.exp(-lambda_p * y) 
-    return Exp_func_body
-
-def F_rand_factory(cdf_func, steps_count = 100000, step_accuracy = 0.1):
+def F_rand_factory(cdf_func, steps_count = 10000, step_accuracy = 0.01):
     """ Define approximation for generated Random Value based on Distribution 
     Function in analitycal form. Return function-closure which generate 
     random values according to defined Cumulative Distribution Function(CDF)
@@ -60,10 +21,10 @@ def F_rand_factory(cdf_func, steps_count = 100000, step_accuracy = 0.1):
     function
         Random function for generate value of Random Value with CDF func_dist
     """
-    y = [0]*steps_count
+    CDF_vals = [0]*steps_count
     cur_arg = 0.0
     for i in range(0, steps_count):
-        y[i] = cdf_func(cur_arg)
+        CDF_vals[i] = cdf_func(cur_arg)
         cur_arg += step_accuracy
     def rand_func_CDF():
         """Function which generate random value for defined CDF based on
@@ -76,21 +37,81 @@ def F_rand_factory(cdf_func, steps_count = 100000, step_accuracy = 0.1):
         number
             value of Random Value with defined CDF
         """
+        def define_interval(f, f_vals, find_index, acc, r_value):
+            """Calculate parameters for defining random value with
+            cumulative distribution function f
+
+            Parameters
+            ----------
+            f : function
+                CDF function necessary distribution
+            f_vals : list
+                tabulated CDF f on some interval
+            find_index : integer
+                index of f_vals such than f_vals[find_index] very closely to r_value
+            acc : number
+                accuracy step - accuracy for finding value of Y: CDF(Y) ~= r_value
+            r_value : number
+                Random value of Uniform Distributed Random Value
+
+            Returns
+            -------
+            tuple (y_k, `y_k+1`, f(y_k), f(`y_k+1`) )
+            """
+            if find_index < 0 or find_index >= len(f_vals):
+                return (None, None, None, None)
+            # DEBUG value type_interval:
+            # 0 - inside f_vals
+            # 1 - outside left f_vals
+            # 2 - outside right f_vals
+            type_interval = 0
+            y_k = find_index * acc
+            y_k_1 = y_k + acc
+            f_k = f_vals[find_index]
+            f_k_1 = 0.0
+            if find_index == 0:
+                if r_value > f_vals[find_index]:
+                    f_k_1 = f_vals[find_index + 1]
+                else:
+                    type_interval = 1
+                    y_k_1 = y_k
+                    y_k -= acc
+                    f_k_1 = f_k
+                    f_k = f(y_k)
+            elif find_index == len(f_vals) - 1:
+                if r_value > f_vals[find_index]:
+                    type_interval = 2
+                    #TODO: make more accurate because true value of CDF(Y) may be
+                    # too distance from f_vals[last_index] value
+                    f_k_1 = f(y_k_1)
+                else:
+                    y_k_1 = y_k
+                    y_k -= acc
+                    f_k_1 = f_k
+                    f_k = f_vals[find_index - 1]
+            elif r_value > f_vals[find_index]:
+                f_k_1 = f_vals[find_index + 1]
+            else:
+                y_k_1 = y_k
+                y_k -= acc
+                f_k_1 = f_k
+                f_k = f_vals[find_index - 1]
+            return y_k, y_k_1, f_k, f_k_1, type_interval
+        
         x = uniform_dist()
         # find nearest y value according x
+        cur_index = steps_count - 1
         borders = [0, steps_count - 1]
         while True:
-            cur_index = (borders[1] + borders[0]) // 2
-            if borders[1] - borders[0] < 2:
-                break
-            if x > y[cur_index]:
+            if x > CDF_vals[cur_index]:
                 borders[0] = cur_index
             else:
                 borders[1] = cur_index
-        find_index = borders[0]
+            if borders[1] - borders[0] < 2:
+                break
+            cur_index = (borders[1] + borders[0]) // 2
         #DEBUG
-        print("find_index = ", find_index)
-        # TODO: when x out of range we need more accuracy
+        #print("find_index = ", cur_index)
         
         # Method of calculation F(x)
         #  was taken from
@@ -104,52 +125,6 @@ def F_rand_factory(cdf_func, steps_count = 100000, step_accuracy = 0.1):
         # MAIN equation: delta_F / delta_y = (x - F(y_k)) / (y - y_k)
         #
         # Hence: y = y_k + (x - F(y_k)) * delta_y / delta_F
-        #DEBUG
-        y_k = step_accuracy * borders[0]
-        # TODO: check why this happen
-        if cdf_func(y_k) > x:
-            # It can only be when approximation interval [0; step_count * step_accuracy] not enough
-            print("error when calculate Random_func by CDF")
-        y_k_p_1 = y_k + step_accuracy
-        ret_val = y_k + ((x - cdf_func(y_k)) * step_accuracy / (cdf_func(y_k_p_1) - cdf_func(y_k)) )
-        return ret_val
+        y_k, y_k_p_1, cdf_k, cdf_k_p_1, type_interval = define_interval(cdf_func, CDF_vals, cur_index, step_accuracy, x)
+        return y_k + ( (x - cdf_k) * step_accuracy / (cdf_k_p_1 - cdf_k)  ), type_interval
     return rand_func_CDF
-
-def test_B_func(gamma):
-    """Function for debug-purpose. Create analitycal Distribution Function
-    Parameters
-    ----------
-
-    Returns
-    -------
-    """
-    B = B_factory(gamma)
-    B_dist = B_rand_factory(B)
-    print("gen_value = ", B_dist())
-
-def Exponential_Flow_Factory(input_lambda):
-    """Factory for define function for generation random value distributed by
-    Exponential Distribution Function. Use library function exp_dist
-    Parameters
-    ----------
-
-    Returns
-    -------
-    function-closure
-        Function for generate random value of Exponental Random Value based on
-        parameter input_lambda
-    """
-    return lambda :exp_dist(1.0/input_lambda)
-
-
-def single_exp_test():
-    input_lambda = 0.5
-    exp_dist_lib = Exponential_Flow_Factory(input_lambda)
-    exp_hand = F_rand_factory(Exp_factory(input_lambda))
-    print("Lib realisation: ", exp_dist_lib())
-    print("Own realisation: ", exp_hand())
-
-if __name__ == "__main__":
-    single_exp_test()
-    print("Finish modeling!")
-
--- lib/test_random.py
+++ lib/test_random.py
@@ -1,131 +1,6 @@
 from numpy.random import exponential as exp_dist
-from numpy.random import random as uniform_dist
-from scipy.stats import norm as norm_distribution
-import math
-
-
-def B_factory(gamma):
-    """Factory for defining distribution function B for distribute
-    serviced time intervals
-
-    Parameters
-    ----------
-    gamma : number
-        TODO: define this parameter
-
-    Returns
-    -------
-    function-closure
-        function B with defined parameter <gamma>
-    """
-    def B_func_body(y):
-        """Distribution function represented in analitical form.
-        This function define some Random value based by Distribution function
-        Parameters
-        ----------
-        y : number
-            TODO: define this parameter
-        Returns
-        number
-            Time-interval necessary for service request
-        """
-        return y**gamma/(1.0 + y**gamma)
-    return B_func_body
-
-def Exp_factory(lambda_p):
-    def Exp_func_body(y):
-        if y < 0.0:
-            return 0.0
-        return 1 - math.exp(-lambda_p * y) 
-    return Exp_func_body
-
-def F_rand_factory(cdf_func, steps_count = 100000, step_accuracy = 0.1):
-    """ Define approximation for generated Random Value based on Distribution 
-    Function in analitycal form. Return function-closure which generate 
-    random values according to defined Cumulative Distribution Function(CDF)
-    Parameters
-    ----------
-    cdf_func : function
-        Define analitycal CDF for Random Value
-    steps_count : integer
-        Internal variable. Define interval on Y-axes for more-closely approximation
-        of ended value for generate Random Value. Besides Y-axes-interval defining by
-        [0, step_counts * step_accuracy] Random Value calculate more rough
-    step_accuracy : number
-        Internal variable. Define accuracy for which on Y-axes was finding values of
-        Random Value
-
-    Returns
-    -------
-    function
-        Random function for generate value of Random Value with CDF func_dist
-    """
-    y = [0]*steps_count
-    cur_arg = 0.0
-    for i in range(0, steps_count):
-        y[i] = cdf_func(cur_arg)
-        cur_arg += step_accuracy
-    def rand_func_CDF():
-        """Function which generate random value for defined CDF based on
-        library Uniform Distribution
-        Parameters
-        ----------
-
-        Returns
-        -------
-        number
-            value of Random Value with defined CDF
-        """
-        x = uniform_dist()
-        # find nearest y value according x
-        borders = [0, steps_count - 1]
-        while True:
-            cur_index = (borders[1] + borders[0]) // 2
-            if borders[1] - borders[0] < 2:
-                break
-            if x > y[cur_index]:
-                borders[0] = cur_index
-            else:
-                borders[1] = cur_index
-        find_index = borders[0]
-        #DEBUG
-        print("find_index = ", find_index)
-        # TODO: when x out of range we need more accuracy
-        
-        # Method of calculation F(x)
-        #  was taken from
-        #  https://www.intuit.ru/studies/courses/643/499/lecture/11355?page=4#sect7
-        # y - random value with distribution function F()
-        # y_k - preliminary calculated value of F()
-        # x - current value of uniform continius distribution
-        # F(y_k) - first value of F() for which: F(y_k) < x < F(y_k+1)
-        # delta_y = y_k+1 - y_k = step_accuracy
-        # delta_F = F(y_k+1) - F(y_k)
-        # MAIN equation: delta_F / delta_y = (x - F(y_k)) / (y - y_k)
-        #
-        # Hence: y = y_k + (x - F(y_k)) * delta_y / delta_F
-        #DEBUG
-        y_k = step_accuracy * borders[0]
-        # TODO: check why this happen
-        if cdf_func(y_k) > x:
-            # It can only be when approximation interval [0; step_count * step_accuracy] not enough
-            print("error when calculate Random_func by CDF")
-        y_k_p_1 = y_k + step_accuracy
-        ret_val = y_k + ((x - cdf_func(y_k)) * step_accuracy / (cdf_func(y_k_p_1) - cdf_func(y_k)) )
-        return ret_val
-    return rand_func_CDF
-
-def test_B_func(gamma):
-    """Function for debug-purpose. Create analitycal Distribution Function
-    Parameters
-    ----------
-
-    Returns
-    -------
-    """
-    B = B_factory(gamma)
-    B_dist = B_rand_factory(B)
-    print("gen_value = ", B_dist())
+import cdf
+import random_by_cdf as cdf_rand
 
 def Exponential_Flow_Factory(input_lambda):
     """Factory for define function for generation random value distributed by
@@ -142,14 +17,28 @@ def Exponential_Flow_Factory(input_lambda):
     return lambda :exp_dist(1.0/input_lambda)
 
 
-def single_exp_test():
+def single_generate_test():
     input_lambda = 0.5
     exp_dist_lib = Exponential_Flow_Factory(input_lambda)
-    exp_hand = F_rand_factory(Exp_factory(input_lambda))
+    exp_hand = cdf_rand.F_rand_factory(cdf.Exp_factory(input_lambda))
     print("Lib realisation: ", exp_dist_lib())
-    print("Own realisation: ", exp_hand())
+    r_value, type_interval = exp_hand()
+    print("Own realisation: ", r_value, "; type = ", type_interval)
+
+def distrib_types(n, input_lambda):
+    # DEBUG test only ontime debugging
+    if n < 1:
+        return None
+    type_distr = {0: 0, 1:0, 2:0}
+    exp_hand = cdf_rand.F_rand_factory(cdf.Exp_factory(input_lambda))
+    for i in range(0, n):
+        r_val, type_interval = exp_hand()
+        if type_interval < 3 and type_interval >= 0:
+            type_distr[type_interval] += 1
+    print(type_distr)
 
 if __name__ == "__main__":
-    single_exp_test()
-    print("Finish modeling!")
+    #single_generate_test()
+    distrib_types(10000, 0.01)
+    print("Finish test Random Value with defined CDF!")
 
