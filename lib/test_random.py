5dfb7086b31e6ec4a2d2b7ad163384533d172adf
Khudyashev Ivan <bahek1983@gmail.com>
2018-11-25 00:34:10 +0700

    Add common plotting CDF by rand CDF

--- lib/test_random.py
+++ lib/test_random.py
@@ -1,30 +1,34 @@
-from numpy.random import exponential as exp_dist
 import cdf
 import random_by_cdf as cdf_rand
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import norm as norm_distribution
+from scipy.stats import gamms as gamma_distribution
 
-def Exponential_Flow_Factory(input_lambda):
-    """Factory for define function for generation random value distributed by
-    Exponential Distribution Function. Use library function exp_dist
-    Parameters
-    ----------
+def draw_expCDF_by_rand(input_lambda):
+    exp_rand = cdf_rand.F_rand_factory(cdf.Exp_factory(input_lambda))
+    draw_CDF_by_randCDF(exp_rand, "Exponential CDF, l = " + str(input_lambda))
 
-    Returns
-    -------
-    function-closure
-        Function for generate random value of Exponental Random Value based on
-        parameter input_lambda
-    """
-    return lambda :exp_dist(1.0/input_lambda)
+def draw_gammaCDF_by_rand(a = 0.9):
+#    gamma_cdf = lambda x:gamma_distribution.cdf(x,
+    pass
 
-def single_generate_test():
-    input_lambda = 1.0
-    exp_dist_lib = Exponential_Flow_Factory(input_lambda)
-    exp_hand = cdf_rand.F_rand_factory(cdf.Exp_factory(input_lambda))
-    print("Lib realisation: ", exp_dist_lib())
-    r_value = exp_hand()
-    print("Own realisation: ", r_value)
+def draw_GaussCDF_by_rand(mu = 1.0, sigma = 0.5):
+    gauss_cdf = lambda x:norm_distribution.cdf(x, mu, sigma)
+    gauss_rand = cdf_rand.F_rand_factory(gauss_cdf)
+    draw_CDF_by_randCDF(gauss_rand, "Gauss CDF, mu = " + str(mu) + ", sigma = " + str(sigma))
+    
+def draw_CDF_by_randCDF(randCDF, plot_title = "Some CDF", figure_title = "CDF"):
+    x, y = tabulate_CDF_by_randCDF(randCDF, False)
+    # build plots
+    plt.plot(x, y, label = plot_title)
+    plt.xlabel("x label")
+    plt.ylabel("y label")
+    plt.title(figure_title)
+    plt.legend()
+    plt.show()
 
-def compare_with_lib_distrib(n, input_lambda):
+def tabulate_CDF_by_randCDF(rand_cdf, is_cdf = True, statistic_volume = 100000, x_max = 10, y_max_scale = 10, delta = 0.1):
     def found_interval(rand_val, delta, points_count):
         found_border = delta
         found_index = 0
@@ -37,44 +41,27 @@ def compare_with_lib_distrib(n, input_lambda):
             found_index = points_count - 1
         return found_index
 
-    import numpy as np
-    import matplotlib.pyplot as plt
-    if n < 1:
+    if statistic_volume < 1:
         return None
-    exp_dist_hand = cdf_rand.F_rand_factory(cdf.Exp_factory(input_lambda))
-    exp_dist_lib = Exponential_Flow_Factory(input_lambda)
     # Initial values for defining plots
-    y_max_scale = 10
-    y_scale_koef = float(y_max_scale) / float(n)
-    x_max = 5
-    delta = 0.1
+    y_scale_koef = float(y_max_scale) / float(statistic_volume)
     points_count = int(x_max / delta)
     x = np.linspace(0, x_max, points_count)
-    y_exp_hand = np.zeros(points_count, dtype = float)
-    y_exp_lib = np.zeros(points_count, dtype = float)
+    y= np.zeros(points_count, dtype = float)
     # experiments to get statistic for plots
-    for i in range(0, n + 1):
-        r_val = exp_dist_hand()
-        y_exp_hand[found_interval(r_val, delta, points_count)] += y_scale_koef
-        r_val = exp_dist_lib()
-        y_exp_lib[found_interval(r_val, delta, points_count)] += y_scale_koef
+    for i in range(0, statistic_volume + 1):
+        r_val = rand_cdf()
+        y[found_interval(r_val, delta, points_count)] += y_scale_koef
     # Accumulate statistic for definint CDF
     #  since CDF(x) = P{X < x}, hence all previous intervals must added to all
     #  next intervals
-    for i in range(1, points_count):
-        y_exp_hand[i] += y_exp_hand[i - 1]
-        y_exp_lib[i] += y_exp_lib[i - 1]
-    # build plots
-    plt.plot(x, y_exp_hand, label = "Hand Exp, l = " + str(input_lambda))
-    plt.plot(x, y_exp_lib, label = "Lib exp, l = " + str(input_lambda))
-    plt.xlabel("x label")
-    plt.ylabel("y label")
-    plt.title("Compare Lib and Hand Realisation. N = " + str(n))
-    plt.legend()
-    plt.show()
+    if is_cdf:
+        for i in range(1, points_count):
+            y[i] += y[i - 1]
+    return (x, y)
 
 if __name__ == "__main__":
-    #single_generate_test()
-    compare_with_lib_distrib(10000, 0.5)
+    #draw_expCDF_by_rand(0.5)
+    draw_GaussCDF_by_rand(3.0, 1.0)
     print("Finish test Random Value with defined CDF!")
 
