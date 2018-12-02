ec23c4d60bb5dda2c9b30d23c96a375e50af8557
Khudyashev Ivan <bahek1983@gmail.com>
2018-12-03 00:03:39 +0700

    Finish plot_core. Add test plot for rand_cdf

--- lib/plot_core.py
+++ lib/plot_core.py
@@ -1,55 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import random_by_cdf as cdf_rand
-import commons
-from cdf import CDF_factory
 
-def 
-
-def draw_expCDF_by_rand(input_lambda):
-    exp_rand = cdf_rand.F_rand_factory(cdf.Exp_factory(input_lambda))
-    draw_CDF_by_randCDF(exp_rand, "Exponential CDF, l = " + str(input_lambda))
-
-def draw_gammaCDF_by_rand(a = 0.9, loc = 0.0, scale = 1.0):
-    """PDF (Probability Density Function) for gamma-distribution defined as
-    PDF(x) = x^(a - 1) * e^(-x/thetta) / ( Г(a) * thetta^a )
-    There is more common case with shift
-    PDF(x) = (x - shift)^(a - 1) * e^(-(x - shift)/thetta) / ( Г(a) * thetta^a )
-    In scipy.stats.gamma this parameters define as:
-    a := a
-    loc := shift
-    scale := thetta
-    CDF has according parameters
-    """
-    gamma_cdf = lambda x:gamma_distribution.cdf(x, a, loc, scale)
-    gamma_rand = cdf_rand.F_rand_factory(gamma_cdf)
-    draw_PDF_by_randCDF(gamma_rand, "Gamma CDF, a = " + str(a) + ",thetta = " + str(scale))
-
-def draw_GaussCDF_by_rand(mu = 1.0, sigma = 0.5):
-    gauss_cdf = lambda x:norm_distribution.cdf(x, mu, sigma)
-    gauss_rand = cdf_rand.F_rand_factory(gauss_cdf)
-    draw_CDF_by_randCDF(gauss_rand, "Gauss CDF, mu = " + str(mu) + ", sigma = " + str(sigma))
-    
-def draw_CDF_by_randCDF(randCDF, plot_title = "Some CDF", figure_title = "CDF"):
-    x, y = tabulate_CDF_by_randCDF(randCDF)
-    # build plots
-    plt.plot(x, y, label = plot_title)
-    plt.xlabel("x label")
-    plt.ylabel("y label")
-    plt.title(figure_title)
-    plt.legend()
-    plt.show()
-
-def draw_PDF_by_randCDF(randCDF, plot_title = "Some CDF", figure_title = "CDF"):
-    x, y = tabulate_CDF_by_randCDF(randCDF, False)
-    plt.plot(x, y, label = plot_title)
-    plt.xlabel("x label")
-    plt.ylabel("y label")
-    plt.title(figure_title)
-    plt.legend()
-    plt.show()
-
-def tabulate_CDF_by_randCDF(rand_cdf, statistic_volume = 100000, is_cdf = True, x_max = 20, y_max_scale = 10, delta = 0.1):
+def tabulate_CDF_by_randCDF(rand_cdf, statistic_volume = 100000, is_cdf, x_max = 20, y_max_scale = 10, delta = 0.1):
     """Tabulate input rand_cdf CDF and build dots (x, y) for plotting inputed CDF
     """
     def found_interval(rand_val, delta, points_count):
@@ -83,6 +35,13 @@ def tabulate_CDF_by_randCDF(rand_cdf, statistic_volume = 100000, is_cdf = True, 
             y[i] += y[i - 1]
     return (x, y)
 
+def find_x_max(cdf_func, epsilon = 1e-3, delta_x = 3.0):
+    x_max = 0.0
+    #TODO: make more intellectual increment
+    while cdf_func(x_max) < 1.0 - epsilon:
+        x_max += delta_x
+    return x_max
+
 def draw_plots(plots, figure_title):
     """Build grafics with all inputed plots
     
@@ -112,10 +71,3 @@ def draw_plots(plots, figure_title):
     plt.title(figure_title)
     plt.legend()
     plt.show()
-
-if __name__ == "__main__":
-    #draw_expCDF_by_rand(0.5)
-    #draw_GaussCDF_by_rand(3.0, 1.0)
-    draw_gammaCDF_by_rand(9.0, 0.0, 0.5)
-    print("Finish test Random Value with defined CDF!")
-
--- lib/test_random.py
+++ lib/test_random.py
@@ -3,16 +3,30 @@ import plot_core
 from cdf import CDF_factory as build_cdf
 from random_by_cdf import F_rand_factory as gen_rand_f
 
-def expCDF_draw_test(cnt_test, l):
-    rand_exp = gen_rand_f(build_cdf(commons.CDF_type.EXPONENTIAL, {"lambda_p": l}))
-    x_cdf, y_cdf = plot_core.tabulate_CDF_by_randCDF(rand_exp, cnt_test)
-    x_pdf, y_pdf = plot_core.tabulate_CDF_by_randCDF(rand_exp, cnt_test, False)
+def gen_exp_cdf(l):
+    return build_cdf(commons.CDF_type.EXPONENTIAL, {"lambda_p": l})
+
+def gen_norm_cdf(mu, sigma):
+    return build_cdf(commons.CDF_type.GAUSS, {"mu": mu, "sigma": sigma})
+
+def CDF_draw_test(cdf_f, cnt_test, common_title):
+    rand_exp = gen_rand_f(cdf_f)
+    x_max = plot_core.find_x_max(cdf_f)
+    x_cdf, y_cdf = plot_core.tabulate_CDF_by_randCDF(rand_exp, cnt_test, True, x_max, 1.0)
+    x_pdf, y_pdf = plot_core.tabulate_CDF_by_randCDF(rand_exp, cnt_test, False, x_max, 1.0)
     plots = [{"x": x_cdf, "y": y_cdf, "title": "CDF"}, {"x": x_pdf, "y": y_pdf, "title": "PDF"}]
-    plot_core.draw_plots(plots, "Exponential Distr with l = " + str(l))
+    plot_core.draw_plots(plots, common_title)
+
+def test_x_max(cdf_f):
+    x_max = plot_core.find_x_max(cdf_f)
+    print("x_max = ", x_max)
 
 if __name__ == "__main__":
-    #draw_expCDF_by_rand(0.5)
-    #draw_GaussCDF_by_rand(3.0, 1.0)
-    draw_gammaCDF_by_rand(9.0, 0.0, 0.5)
+    l = 0.5
+    #test_x_max(gen_exp_cdf(l))
+    CDF_draw_test(gen_exp_cdf(l), 100000, "Exponential with l = " + str(l))
+    #mu = 1.0
+    #sigma = 0.1
+    #CDF_draw_test(gen_norm_cdf(mu, sigma), 100000, "Gauss with mu = " + str(mu) + ", sigma = " + str(sigma))
     print("Finish test Random Value with defined CDF!")
 
